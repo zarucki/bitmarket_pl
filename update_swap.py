212c86f9f18d5ecbe27d498c940eb27529708ca5
zarucki <zarucki@gmail.com>
2016-06-04 17:06:48 -0400

    Extracted logging setup to method.

--- update_swap.py
+++ update_swap.py
@@ -8,17 +8,21 @@ import traceback
 import logging
 from pprint import pprint
 
-logger = logging.getLogger('update_swap')
-logger.setLevel(logging.INFO)
-fh = logging.FileHandler('update_swap.log')
-fh.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-logger.addHandler(fh)
-logger.addHandler(ch)
+def setupLogger():
+	logger = logging.getLogger('update_swap')
+	logger.setLevel(logging.INFO)
+	fh = logging.FileHandler('update_swap.log')
+	fh.setLevel(logging.INFO)
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.INFO)
+	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+	fh.setFormatter(formatter)
+	ch.setFormatter(formatter)
+	logger.addHandler(fh)
+	logger.addHandler(ch)
+	return logger
+
+logger = setupLogger()
 
 configFile = open('config.json', 'r')
 config = json.load(configFile)
@@ -30,6 +34,7 @@ secretKey = str(config['secret_api_key'])
 offsetFromCutoff = float(config['offset_from_cuttof'])
 
 logger.info('SleepTime: ' + str(sleepTimeBetweenChecks) + ' PublicKey: ' + publicKey + ' SecretKey: ' + secretKey + ' cutoff_ofset: ' + str(offsetFromCutoff))
+
 
 def mergeTwoDicts(x, y):
 	z = x.copy()
